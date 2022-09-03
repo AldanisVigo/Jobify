62c9fc3a801d3f2e9fa2f86006148921bc2e52b1
Aldanis Vigo <aldanisvigo@gmail.com>
2022-09-03 02:38:32 -0500

    Added search functionality to the All Jobs page

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -28,7 +28,8 @@ import {
     EDIT_JOB_SUCCESS,
     EDIT_JOB_ERROR,
     SHOW_STATS_BEGIN,
-    SHOW_STATS_SUCCESS
+    SHOW_STATS_SUCCESS,
+    CLEAR_FILTERS
 } from './actions'
 import axios from 'axios'
 
@@ -67,7 +68,12 @@ const initialState = {
     numPages : 1,
     page : 1,
     stats : {},
-    monthlyApplications : []
+    monthlyApplications : [],
+    search : '',
+    searchStatus : 'all',
+    searchType : 'all',
+    sort : 'latest',
+    sortOptions : ['latest', 'oldest', 'a-z', 'z-a']
 }
 
 const AppContext = React.createContext()
@@ -299,7 +305,12 @@ const AppProvider = ({children}) => {
     }
 
     const getJobs = async () => {
-        let url = '/jobs'
+        const { search, searchStatus, searchType, sort } = state
+        let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}`
+
+        if(search){
+            url = url + `&search=${search}`
+        }
 
         dispatch({
             type : GET_JOBS_BEGIN
@@ -420,6 +431,13 @@ const AppProvider = ({children}) => {
         }
     }
 
+    const clearFilters = () => {
+        console.log("clear filters")
+        dispatch({
+            type : CLEAR_FILTERS
+        })
+    }
+
     return <AppContext.Provider value={{
         ...state,
         displayAlert,
@@ -435,7 +453,8 @@ const AppProvider = ({children}) => {
         setEditJob,
         deleteJob,
         editJob,
-        showStats
+        showStats,
+        clearFilters
     }}>
         {children}
     </AppContext.Provider>
